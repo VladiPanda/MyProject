0cddfa6ee50afadce1322ddf20bed6927a4761d7
Vladimir Trofimov <trofimov57@mail.ru>
2021-10-08 16:00:28 +0300

    Initial commit

---
+++ MyProject/Presenter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyProject
+{
+    class Presenter
+    {
+        Model model;
+        MainWindow mainWindow;
+
+        public Presenter(MainWindow mainWindow)
+        {
+            this.mainWindow = mainWindow;
+            this.model = new Model();
+            this.mainWindow.SomeEvent += MainWindow_SomeEvent;
+
+        }
+
+         void MainWindow_SomeEvent(object sender, EventArgs e)
+        {
+            this.mainWindow.TextBox1.Text = this.model.SomeMethod(this.mainWindow.TextBox1.Text);
+        }
+    }
+}
